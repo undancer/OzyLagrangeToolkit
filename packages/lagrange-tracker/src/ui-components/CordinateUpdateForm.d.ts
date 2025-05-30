c5e743ea80f7db4d224419ffd71a994607fc4ead
DESKTOP-C5QDJQH\undancer <undancer@gmail.com>
2025-05-30 15:54:55 +0800

    feat: 将原项目移动到子目录中，添加若干个其他项目的模板

--- packages/lagrange-tracker/src/ui-components/CordinateUpdateForm.d.ts
+++ packages/lagrange-tracker/src/ui-components/CordinateUpdateForm.d.ts
@@ -6,7 +6,7 @@
 
 import * as React from "react";
 import { GridProps, TextFieldProps } from "@aws-amplify/ui-react";
-import { Cordinate } from "../API.ts";
+import { Cordinate } from "../API.js";
 export declare type EscapeHatchProps = {
     [elementHierarchy: string]: Record<string, unknown>;
 } | null;
